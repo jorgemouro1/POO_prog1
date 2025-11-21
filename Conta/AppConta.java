6943bea615eb34b04e2bc1bf9ee19f11ab3534a3
jorgemouro1 <mourocantorna@gmail.com>
2025-11-21 11:52:54 +0100

    tonecho

--- Conta/AppConta.java
+++ Conta/AppConta.java
@@ -3,11 +3,20 @@ package Conta;
 public class AppConta {
   public static void main(String[] args) {
     // creamos o obxecto usando o constructor
-    Conta c = new Conta(01, "Tonecho", 1000); // creamos o obxecto C co constructor por defecto
+    Conta c = new Conta(1, "Tonecho", 1000); // creamos o obxecto C co constructor por defecto
+
+    Conta conta2 = new Conta();
+    conta2.idConta = 2;
+    conta2.titular = "Jorge";
+    conta2.saldo = 100000;
 
     // mostramos resultados
     System.out.println(c.idConta);
     System.out.println(c.saldo);
     System.out.println(c.titular);
+
+    System.out.println(conta2.idConta);
+    System.out.println(conta2.saldo);
+    System.out.println(conta2.titular);
   }
 }
